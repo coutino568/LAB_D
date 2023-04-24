3e6d9f61b31bb200e31fe402b9862dd07c3e8977
coutino568 <40869460+coutino568@users.noreply.github.com>
2023-04-24 01:04:35 -0600

    remove comments

--- LAB_D.py
+++ LAB_D.py
@@ -5,19 +5,40 @@ filePath3 = "slr-3.yal"
 filePath4 = "slr-4.yal"
 
 tokens = []
-test= "jojo(*jojo*) jeje(*jejeje*)jajaja"
+test= "parte1(*comentario1*)parte2(*comentario2*)parte3 \n parte4 \n parte5"
 
 openining_symbol = "(*"
 closing_symbol = "*)"
 
 def removeComments(original , openining_symbol, closing_symbol):
 	result = ""
+	lastStart=0
+	lastEnd=0
 	for character_id in range (0, len(original)):
 		#print(original[character_id])
 		if (original[character_id] == "(" and original[character_id+1]=="*"):
-			print (" ENCONTRE (*")
-			
-		result
+			#print (" ENCONTRE (*")
+			for x in range (character_id, len(original)):
+				if (original[x]=="*" and original[x+1]==")"):
+					##incluir todo antes del comentario
+
+					for j in range (lastStart, character_id):
+						result +=(original[j])
+					lastStart=x+2
+					##incluir todo despues del comentario
+					# for k in range (x, len(original)):
+					# 	result+=(original[k])
+
+					#print("RESULT SO FAR:  \n")
+					#print(result)
+					break
+	##incluir todo despues del ultimo comentario
+	for l in range (lastStart, len(original)):
+		result=result+original[l]
+
+
+
+	#print(result)
 		
 		
 
@@ -36,11 +57,11 @@ file = open(filePath1, "r")
 
 
 original_string = file.read()
-print(original_string)
-# clean = removeComments(original_string , openining_symbol , closing_symbol)
-# print (clean)
-clean = removeComments(test , openining_symbol , closing_symbol)
+#print(original_string)
+clean = removeComments(original_string , openining_symbol , closing_symbol)
 print (clean)
+# clean = removeComments(test , openining_symbol , closing_symbol)
+# print (clean)
 
 def read():
 	pass
